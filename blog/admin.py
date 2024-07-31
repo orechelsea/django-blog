2a6ab56af7db8f500d70d6591b498f377b3d6ecd
Chelsea Osayomi <orechelsea04@gmail.com>
2024-07-31 17:25:20 +0000

    "added the css for the html"

--- blog/admin.py
+++ blog/admin.py
@@ -7,7 +7,7 @@ class PostAdmin(SummernoteModelAdmin):
     
     list_display = ('title', 'slug', 'status')
     search_fields = ['title']
-    list_filter = ('status',)
+    list_filter = ('status', 'created_on',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 # Register your models here.
